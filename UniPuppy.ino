79749d6434e68f498c5fffa697eb0509157aebb4
npnkhoi <npnkhoi@gmail.com>
2019-12-19 16:21:10 +0700

    Update UniPuppy.ino

--- UniPuppy.ino
+++ UniPuppy.ino
@@ -15,10 +15,6 @@ const int IN8 = 8;
 
 const int MIN_SPEED = 0;
 const int MAX_SPEED = 255;
-const int SPEED_1 = 90;
-const int SPEED_2 = 125;
-const int SPEED_3 = 125;
-const int SPEED_4 = 90;
 const int N_DIST = 8;
 const int TURN_DIST = 50;
 
@@ -28,121 +24,41 @@ int idx_dist = 0;
 SoftwareSerial mySoftwareSerial(12, 13); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 void printDetail(uint8_t type, int value);
-//int current_song = BARK_AUD;
 
-void setup()
-{
-  // init distance sensor
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  Serial.begin(9600);
+// --------------------------------------------------------- END OF CONSTANTS -------------------------------------
 
-  // init speaker
-  mySoftwareSerial.begin(9600);
-  Serial.println();
-  Serial.println(F("DFRobot DFPlayer Mini Demo"));
-  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+void go_forward(int speed) {
+  constrain(speed, MIN_SPEED, MAX_SPEED);
   
-  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
-    Serial.println(F("Unable to begin:"));
-    Serial.println(F("1.Please recheck the connection!"));
-    Serial.println(F("2.Please insert the SD card!"));
-    while(true){
-      delay(0); // Code to compatible with ESP8266 watch dog.
-    }
-  }
-  Serial.println(F("DFPlayer Mini online."));
-  myDFPlayer.volume(30);
-  myDFPlayer.loop(2);
-
-
-  // init motor
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(IN5, OUTPUT);
-  pinMode(IN6, OUTPUT);
-  pinMode(IN7, OUTPUT);
-  pinMode(IN8, OUTPUT);
-  int speed0 = 100;
-  motor_1_up(SPEED_1);
-//  delay(1000);
-  motor_2_up(SPEED_2);
-//  delay(1000);
-  motor_3_up(SPEED_3);
-//  delay(1000);
-  motor_4_up(SPEED_4);  
-}
-
-void motor_1_stop() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-}
- 
-void motor_2_stop() {
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-}
- 
-void motor_1_up(int speed) { //speed: từ 0 - MAX_SPEED
-  Serial.println("Motor 1 da up");
-  speed = constrain(speed, MIN_SPEED, MAX_SPEED);//đảm báo giá trị nằm trong một khoảng từ 0 - MAX_SPEED - http://arduino.vn/reference/constrain
   digitalWrite(IN1, HIGH);// chân này không có PWM
   analogWrite(IN2, 255 - speed);
-}
- 
-void motor_1_down(int speed) {
-  speed = constrain(speed, MIN_SPEED, MAX_SPEED);//đảm báo giá trị nằm trong một khoảng từ 0 - MAX_SPEED - http://arduino.vn/reference/constrain
-  digitalWrite(IN1, LOW);// chân này không có PWM
-  analogWrite(IN2, speed);
-}
- 
-void motor_2_up(int speed) { //speed: từ 0 - MAX_SPEED
-  speed = constrain(speed, MIN_SPEED, MAX_SPEED);//đảm báo giá trị nằm trong một khoảng từ 0 - MAX_SPEED - http://arduino.vn/reference/constrain
   analogWrite(IN3, speed);
   digitalWrite(IN4, LOW);// chân này không có PWM
-}
- 
-void motor_2_down(int speed) {
-  speed = constrain(speed, MIN_SPEED, MAX_SPEED);//đảm báo giá trị nằm trong một khoảng từ 0 - MAX_SPEED - http://arduino.vn/reference/constrain
-  analogWrite(IN4, 255 - speed);
-  digitalWrite(IN3, HIGH);// chân này không có PWM
-}
-
-void motor_3_stop() {
-  digitalWrite(IN5, LOW);
-  digitalWrite(IN6, LOW);
-}
- 
-void motor_4_stop() {
-  digitalWrite(IN7, LOW);
-  digitalWrite(IN8, LOW);
-}
- 
-void motor_3_up(int speed) { //speed: từ 0 - MAX_SPEED
-  Serial.println("Motor 1 da up");
-  speed = constrain(speed, MIN_SPEED, MAX_SPEED);//đảm báo giá trị nằm trong một khoảng từ 0 - MAX_SPEED - http://arduino.vn/reference/constrain
+  
   digitalWrite(IN5, HIGH);// chân này không có PWM
   analogWrite(IN6, 255 - speed);
-}
- 
-void motor_3_down(int speed) {
-  speed = constrain(speed, MIN_SPEED, MAX_SPEED);//đảm báo giá trị nằm trong một khoảng từ 0 - MAX_SPEED - http://arduino.vn/reference/constrain
-  digitalWrite(IN5, LOW);// chân này không có PWM
-  analogWrite(IN6, speed);
-}
- 
-void motor_4_up(int speed) { //speed: từ 0 - MAX_SPEED
-  speed = constrain(speed, MIN_SPEED, MAX_SPEED);//đảm báo giá trị nằm trong một khoảng từ 0 - MAX_SPEED - http://arduino.vn/reference/constrain
   analogWrite(IN7, speed);
   digitalWrite(IN8, LOW);// chân này không có PWM
 }
- 
-void motor_4_down(int speed) {
-  speed = constrain(speed, MIN_SPEED, MAX_SPEED);//đảm báo giá trị nằm trong một khoảng từ 0 - MAX_SPEED - http://arduino.vn/reference/constrain
-  analogWrite(IN8, 255 - speed);
-  digitalWrite(IN7, HIGH);// chân này không có PWM
+
+void go_backward(int speed) {
+  constrain(speed, MIN_SPEED, MAX_SPEED);
+  
+  digitalWrite(IN2, HIGH);// chân này không có PWM
+  analogWrite(IN1, 255 - speed);
+  analogWrite(IN4, speed);
+  digitalWrite(IN3, LOW);// chân này không có PWM
+  
+  digitalWrite(IN6, HIGH);// chân này không có PWM
+  analogWrite(IN5, 255 - speed);
+  analogWrite(IN8, speed);
+  digitalWrite(IN7, LOW);// chân này không có PWM
+}
+
+void stop() {
+  for (int i = 4; i <= 11; ++ i) {
+    digitalWrite(i, LOW);
+  }
 }
 
 double get_distance() {
@@ -230,29 +146,57 @@ void printDetail(uint8_t type, int value){
   }
 }
 
-int current_status = 1; // RUNNING
-int speed0 = 100;
+// ------------------------------------ MAIN FUNCTIONS ----------------------------------------------------
+
+void setup()
+{
+  // init distance sensor
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  Serial.begin(9600);
+
+  // init speaker
+  mySoftwareSerial.begin(9600);
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true){
+      delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }
+  Serial.println(F("DFPlayer Mini online."));
+
+  // init motor
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(IN5, OUTPUT);
+  pinMode(IN6, OUTPUT);
+  pinMode(IN7, OUTPUT);
+  pinMode(IN8, OUTPUT);
+  
+  // initialize
+  myDFPlayer.volume(30);
+  myDFPlayer.enableLoopAll();
+  go_forward(100);
+}
+
+bool current = true; // NOT MOVE
 
 void loop() {
   if (get_distance() <= TURN_DIST) {
-    motor_1_stop();
-    motor_2_stop();
-    motor_3_stop();
-    motor_4_stop();
-    delay(2000);
-    motor_1_up(SPEED_1);
-    motor_2_down(SPEED_2);
-    motor_3_down(SPEED_3);
-    motor_4_up(SPEED_4);
-    delay(1000);
-    motor_1_stop();
-    motor_2_stop();
-    motor_3_stop();
-    motor_4_stop();
-  } else {
-    motor_1_up(SPEED_1);
-    motor_2_up(SPEED_2);
-    motor_3_up(SPEED_3);
-    motor_4_up(SPEED_4);
-  }
+    if (current) {
+      stop();
+      current = false; 
+    }
+  } else if (!current) {
+    go_forward(100);
+    current = true;
+  }  
 }
